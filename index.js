5ca95f9519d92132bf02bdb57394d4a85f11a7ca
BabulKumar2022 <bb3583400@gmail.com>
2022-05-02 12:15:14 +0600

    b

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 5000;
 
-
+//  show on browser
 app.get('/', (req, res)  =>{
     res.send('hello server is running')
 })
 
-
+// show on terminal
 app.listen(port, () =>{
     console.log('listen to port', port)
 })

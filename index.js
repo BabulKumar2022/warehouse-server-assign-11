d539115416ea3c17624e802589287a3858714ac0
BabulKumar2022 <bb3583400@gmail.com>
2022-05-04 21:20:50 +0600

    server connected to DB

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+// var MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port =process.env.PORT || 5000;
+
 // middle ware
 app.use(cors ());
 app.use(express.json());
 
-
-const laptops =[
-    {id: 1, name: 'HP Pro', processor: 'Cor i5', price: '$3200'},
-    {id: 2, name: 'ASUS Pro', processor: 'Cor i3', price: '$2200'},
-    {id: 3, name: 'ACEAR Pro', processor: 'Cor i5', price: '$2000'},
-    {id: 4, name: 'DELL Pro', processor: 'Cor i7', price: '$4000'},
-    {id: 5, name: 'DELL Pro', processor: 'Cor i7', price: '$4000'}
-]
-
-
-
-
-
-//  show on browser
-app.get('/', (req, res)  =>{
-    res.send('hello server is running')
-})
 
 app.get('/laptop', (req, res) =>{
     res.send(laptops);
@@ -44,7 +31,35 @@ app.post('/laptop', (req, res) =>{
 })
 
 
-// show on terminal
+
+//--------------------------------------------------------------------
+// user=john67
+//pass =WcsjTy6PHL4Dgj2g
+
+
+
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.vhhpf.mongodb.net:27017,cluster0-shard-00-01.vhhpf.mongodb.net:27017,cluster0-shard-00-02.vhhpf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-rxrbhl-shard-0&authSource=admin&retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+try{
+  await client.connect();
+  const productCollection = client.db('newass11db').collection('laptops');
+  app.get('/laptops', async(req, res) =>{
+    const query ={};
+    const cursor = productCollection.find(query);
+    const laptops =await cursor.toArray();
+    res.send(laptops);
+  })
+}finally{}
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('ass11 server is running');
+})
 app.listen(port, () =>{
-    console.log('listen to port', port)
+    console.log('ass11 running on port', port);
 })

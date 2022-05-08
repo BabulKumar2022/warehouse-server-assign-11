1daa2f3c28e7b6a540d8df950d765475e6b356df
BabulKumar2022 <bb3583400@gmail.com>
2022-05-08 16:40:55 +0600

    checked

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ finally{}
 run().catch(console.dir);
 
 app.get('/', (req, res) =>{
-    res.send('ass11 server is running but not work');
+    res.send('ass11 server is running and heroku is working');
 })
 app.listen(port, () =>{
     console.log('ass11 running  on port', port);
